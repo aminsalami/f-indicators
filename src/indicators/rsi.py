205a2d3427468eaf2b72e834b376839b6ad80b95
aminsalami <aminsalami@pm.me>
2020-05-09 15:08:18 +0430

    Add price to RSIIndicator

--- src/indicators/rsi.py
+++ src/indicators/rsi.py
@@ -8,6 +8,7 @@ class RSIParameter(object):
         self.bottom = random.randint(1, 50)
         self.top = random.randint(50, 100)
         self.period_A = random.randint(2, 30)
+        self.price = random.choice(["Open", "Close", "Low", "High"])
 
     def xover(self, obj):
         """
@@ -21,16 +22,19 @@ class RSIParameter(object):
             p.bottom = self.bottom
             p.top = obj.top if obj.top > self.bottom else random.randint(min(self.bottom, 50), self.top)
             p.period_A = self.period_A
+            p.price = obj.price
 
         elif tmp < 60:
             p.bottom = obj.bottom
             p.top = self.top if self.top > obj.bottom else random.randint(max(self.top, 50), 100)
             p.period_A = obj.period_A
+            p.price = self.price
 
         else:
-            p.bottom = self.bottom + random.choice([4, -4])
-            p.top = self.top + random.choice([4, -4])
+            p.bottom = self.bottom + random.indint(-4, 4)
+            p.top = self.top + random.randint(-4, 4)
             p.period_A = random.randint(min(self.period_A, obj.period_A), max(self.period_A, obj.period_A))
+            p.price = random.choice(["Open", "Close", "Low", "High"])
 
             if p.top <= p.bottom:
                 p.top = random.randint(p.bottom, 100)
@@ -56,14 +60,15 @@ class RSIIndicator(BaseStrategy):
     params = RSIParameter
 
     def next(self) -> int:
-        if self.rsi_A[-1] < self.params_RSI.bottom and self.rsi_A[-2] < self.params_RSI.bottom:
+        if self.rsi_A[-1] < self.params_RSI.bottom:
             # buy
             return 1
-        elif self.rsi_A[-1] > self.params_RSI.top and self.rsi_A[-2] > self.params_RSI.top:
+        elif self.rsi_A[-1] > self.params_RSI.top:
             # sell
             return -1
         else:
             return 0
 
     def init(self):
-        self.rsi_A = self.I(RSI, self.data['Close'], self.params_RSI.period_A)
+        price = self.params_RSI.price
+        self.rsi_A = self.I(RSI, self.data[price], self.params_RSI.period_A)
