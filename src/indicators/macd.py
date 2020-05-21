c412266392575fb0af7f865bb28af8f86f7a8a12
aminsalami <aminsalami@pm.me>
2020-05-21 23:59:10 +0430

    Separate High, Low indicators

--- src/indicators/macd.py
+++ src/indicators/macd.py
@@ -3,6 +3,15 @@ from talib import MACD
 from base import BaseStrategy
 
 
+
+# h_macd_slow_period = 15
+# h_macd_fast_period = 6
+# h_macd_tresh = 0.0001
+
+# l_macd_slow_period = 19
+# l_macd_fast_period = 5
+# l_macd_tresh = -0.00013
+
 class MACDParameter(object):
     """
     MACD Line: The MACD line is the heart of the indicator and by default it’s the
@@ -14,9 +23,16 @@ class MACDParameter(object):
     MACD Histogram: MACD Line – Signal Line
     """
     def __init__(self):
-        self.bottom = random.randint(4, 10)             # fast_period
-        self.top = random.randint(self.bottom, 20)      # slow_period
         self.period = 9
+        # Low
+        self.lbottom = random.randint(2, 8)     # fast_period
+        self.ltop = random.randint(16, 22)      # slow_period
+        self.ltresh = -0.00013 + random.randint(-6, +6)/200000
+
+        # High
+        self.hbottom = random.randint(3, 9)     # fast_period
+        self.htop = random.randint(12, 18)      # slow_period
+        self.htresh = 0.0001 + random.randint(-6, +6)/200000
 
     def xover(self, obj):
         """
@@ -27,32 +43,58 @@ class MACDParameter(object):
         p = MACDParameter()
         tmp = random.randint(1, 100)
         if tmp < 30:
-            p.bottom = self.bottom
-            p.top = obj.top if obj.top > self.bottom else random.randint(min(self.bottom, 20), self.top)
+            p.ltresh = self.ltresh
+            p.htresh = self.htresh
+            p.lbottom = self.lbottom
+            p.hbottom = self.hbottom
+
+            p.ltop = obj.ltop if obj.ltop > self.lbottom else random.randint(min(self.lbottom, 8), self.ltop)
+            p.htop = obj.htop if obj.htop > self.hbottom else random.randint(min(self.hbottom, 9), self.htop)
             p.period = self.period
 
         elif tmp < 60:
-            p.bottom = obj.bottom
-            p.top = self.top if self.top > obj.bottom else random.randint(p.bottom, 20)
+            p.ltresh = obj.ltresh
+            p.htresh = obj.htresh
+            p.lbottom = obj.lbottom
+            p.hbottom = obj.hbottom
+
+            p.ltop = self.ltop if self.ltop > obj.lbottom else random.randint(p.lbottom, 8)
+            p.htop = self.htop if self.htop > obj.hbottom else random.randint(p.hbottom, 9)
             p.period = obj.period
 
         else:
-            p.bottom = self.bottom + random.randint(-1, 1)
-            if p.bottom < 2:
-                p.bottom = 2
-            p.top = self.top + random.randint(-1, 1)
+            p.htresh =  self.htresh + random.randint(-3, +3)/100000
+            p.ltresh =  self.ltresh + random.randint(-3, +3)/100000
+            p.lbottom = self.lbottom + random.randint(-1, 1)
+            p.hbottom = self.hbottom + random.randint(-1, 1)
+            if p.lbottom < 2:
+                p.lbottom = 2
+            if p.hbottom < 2:
+                p.hbottom = 2
+            p.ltop = self.ltop + random.randint(-1, 1)
             p.period = random.randint(min(self.period, obj.period), max(self.period, obj.period))
-            if p.top <= p.bottom:
-                p.top = random.randint(p.bottom, 20)
+            if p.ltop <= p.lbottom:
+                p.ltop = random.randint(p.lbottom, 22)
+            if p.htop <= p.hbottom:
+                p.htop = random.randint(p.hbottom, 18)
 
         return p
 
     def mutate(self):
-        return
+        self.ltresh = -0.00013 + random.randint(-6, +6)/200000
+        self.htresh = 0.0001 + random.randint(-6, +6)/200000
+        self.lbottom = random.randint(2, 8)
+        self.hbottom = random.randint(3, 9)
+        self.ltop = self.ltop + random.randint(-1, 1)
+        self.htop = self.htop + random.randint(-1, 1)
+        if self.ltop <= self.lbottom:
+            self.ltop = random.randint(self.lbottom, 22)
+        if self.htop <= self.hbottom:
+            self.htop = random.randint(self.hbottom, 18)
 
     def get_params(self):
         # (fast_period, slow_period, signal_period)
-        return ("MACD", self.bottom, self.top, self.period)
+        return ("MACD", self.lbottom, self.ltop, self.hbottom, self.htop, self.ltresh, self.htresh)
 
 
 class MACDIndicator(BaseStrategy):
@@ -63,11 +105,11 @@ class MACDIndicator(BaseStrategy):
     params = MACDParameter
 
     def next(self) -> int:
-        if not self.bought and self.macd[-1] < -0.00013:
+        if not self.bought and self.lmacd[-1] < self.params_MACD.ltresh:
             # Buy
             self.bought = not self.bought
             return 1
-        elif self.bought and self.macd[-1] < 0.00013 and self.macd[-2] > 0.00013:
+        elif self.bought and self.hmacd[-1] < self.params_MACD.htresh and self.hmacd[-2] > self.params_MACD.htresh:
             # Sell
             self.bought = not self.bought
             return -1
@@ -76,4 +118,5 @@ class MACDIndicator(BaseStrategy):
 
     def init(self):
         self.bought = False
-        self.macd, _, _ = self.I(MACD, self.data['Close'], self.params_MACD.bottom, self.params_MACD.top, self.params_MACD.period)
+        self.lmacd, _, _ = self.I(MACD, self.data['Low'], self.params_MACD.lbottom, self.params_MACD.ltop, self.params_MACD.period)
+        self.hmacd, _, _ = self.I(MACD, self.data['High'], self.params_MACD.hbottom, self.params_MACD.htop, self.params_MACD.period)
--- src/indicators/rsi.py
+++ src/indicators/rsi.py
@@ -3,12 +3,21 @@ from talib import RSI
 from base import BaseStrategy
 
 
+# h_rsi_candles = 3
+# h_rsi_tresh = 70
+# l_rsi_candles = 6
+# l_rsi_tresh = 30
+
+MIN1 = 25
+MIN2 = 35
+MAX1 = 65
+MAX2 = 75
+
 class RSIParameter(object):
     def __init__(self):
-        self.bottom = 40
-        self.top = 50
+        self.bottom = random.randint(MIN1, MIN2)
+        self.top = random.randint(MAX1, MAX2)
         self.period_A = 14
-        self.price = random.choice(["Open", "Close", "Low", "High"])
 
     def xover(self, obj):
         """
@@ -17,15 +26,25 @@ class RSIParameter(object):
         NOTE: The `obj` has lower fitness
         """
         tmp = random.randint(1, 100)
-        if tmp > 60:
-            return obj
-        return self
+        p = RSIParameter()
+        if tmp < 30:
+            p.bottom = self.bottom
+            p.top = obj.top
+        elif tmp < 60:
+            p.bottom = obj.bottom
+            p.top = self.top
+        else:
+            p.bottom = self.bottom + random.randint(-2, +2)
+            p.top = self.top + random.randint(-2, +2)
+
+        return p
 
     def mutate(self):
-        self.price = random.choice(["Open", "Close", "Low", "High"])
+        self.bottom = random.randint(MIN1, MIN2)
+        self.top = random.randint(MAX1, MAX2)
 
     def get_params(self):
-        return ("RSI", self.bottom, self.top, self.period_A, self.price)
+        return ("RSI", self.bottom, self.top, self.period_A)
 
     def check_distance(self, bottom, top, min_distance=10):
         while top - bottom < min_distance:
@@ -44,15 +63,15 @@ class RSIIndicator(BaseStrategy):
     params = RSIParameter
 
     def next(self) -> int:
-        if self.rsi_A[-1] < self.params_RSI.bottom:
+        if self.lrsi[-1] < self.params_RSI.bottom:
             # buy
             return 1
-        elif self.rsi_A[-1] > self.params_RSI.top:
+        elif self.hrsi[-1] > self.params_RSI.top:
             # sell
             return -1
         else:
             return 0
 
     def init(self):
-        price = self.params_RSI.price
-        self.rsi_A = self.I(RSI, self.data[price], self.params_RSI.period_A)
+        self.lrsi = self.I(RSI, self.data["Low"], 6)
+        self.hrsi = self.I(RSI, self.data["High"], 3)
